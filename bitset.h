3474f0faf5faa980faca327de7e8f27c076a2f69
hiraditya <hiraditya@gmail.com>
2015-03-18 07:44:14 -0500

    Defined set of APIs for the bitset.

--- bitset.h
+++ bitset.h
@@ -22,6 +22,39 @@ class bitset {
   unsigned bitset_capacity;
 
 public:
+
+  /* Reference to a single bit.
+     This is useful for indexing a bit in the bitset. */
+  class ref
+  {
+    friend class bitset;
+    unsigned idx;
+    bits_ptr_t *bitword_ptr;
+
+    /* Cannot use the default constructor. */
+    ref();
+  
+  public:
+    /* Reference to the i-th bit in the bitset. */
+    ref(bitset *b, unsigned i)
+    {
+    }
+
+    ref(const ref&) = default;
+
+    ref &operator=(ref t)
+    {
+    }
+
+    ref &operator=(bool t)
+    {
+    }
+
+    operator bool() const
+    {
+    }
+  };
+
   bitset()
       : bits_ptr(&bits_stack), bits_stack(0),
         bitset_size(BITS_STACK_SIZE), bitset_capacity(bitset_size)
@@ -51,5 +84,109 @@ public:
   {
     memset(p, 0 - (int)v, capacity);
   }
+
+  /* Returns size in bytes */
+  unsigned size() const
+  {
+    return bitset_size;
+  }
+
+  /* Returns capacity in bytes */
+  unsigned capacity() const
+  {
+    return bitset_capacity;
+  }
+
+  /* Invert one bit in the bitset. */
+  void invert(unsigned i)
+  {
+  }
+
+  /* Invert a range of bits in the bitset. */
+  void invert(unsigned i, unsigned j)
+  {
+  }
+
+  /* Invert all the bits in the bitset. */
+  void invert()
+  {
+  }
+
+  /* Set one bit in the bitset. */
+  void set(unsigned i)
+  {
+  }
+
+  /* Set a range of bits in the bitset. */
+  void set(unsigned i, unsigned j)
+  {
+  }
+
+  /* Set all the bits in the bitset. */
+  void set()
+  {
+  }
+
+  /* Reset one bit in the bitset. */
+  void reset(unsigned i)
+  {
+  }
+
+  /* Reset a range of bits in the bitset. */
+  void reset(unsigned i, unsigned j)
+  {
+  }
+
+  /* Reset all the bits in the bitset. */
+  void reset()
+  {
+  }
+
+  /* Return true if all bits in the bitset are set (=1). */
+  bool all() const
+  {
+  }
+
+  /* Return true if any bit in the bitset is set (=1). */
+  bool any() const
+  {
+  }
+
+  /* Indexing operator defined in terms of class ref. */
+  ref operator[](unsigned i)
+  {
+  }
+
+  bool operator[](unsigned i) const
+  {
+  }
+
+  const bitset& operator=(const bitset& b)
+  {
+  }
+
+  /* Equality comparison. */
+  bool operator==(const bitset& b) const
+  {
+  }
+
+  bool operator!=(const bitset& b) const
+  {
+    return !(*this == b);
+  }
+
+  /* Logical operations. */
+  bitset& operator|=(const bitset& b)
+  {
+  }
+
+  bitset& operator&=(const bitset& b)
+  {
+  }
+
+  bitset& operator^=(const bitset& b)
+  {
+  }
+
 };
 
